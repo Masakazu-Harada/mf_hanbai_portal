d86bb36f8e66f9eab75da5436425f3d92614e121
Masakazu_Harada <seungri.uhm@gmail.com>
2022-12-17 10:03:47 +0900

    ルーティング手直し

--- config/initializers/mf_hanbai.rb
+++ config/initializers/mf_hanbai.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do 
   config.mf_hanbai_portal = {
-    staff: { host: "mf_hanbai_portal.example.com", path: "" },
-    admin: { host: "mf_hanbai_portal.example.com", path: "admin" },
+    staff: { host: "mf-hanbai-portal.example.com", path: "" },
+    admin: { host: "mf-hanbai-portal.example.com", path: "admin" },
     customer: { host: "example.com", path: "mypage" },
   }
 end

4294370d7e446300681773082fb6dd7b1448a4d2
Masakazu_Harada <seungri.uhm@gmail.com>
2022-12-19 21:01:09 +0900

    400 Bad Request

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -3,6 +3,8 @@ module ErrorHandlers
 
   included do
     rescue_from StandardError, with: :rescue500
+    rescue_from ApplicationController::Forbidden, with: :rescue403
+    rescue_from ApplicationController::IpAddressRejected, with: :rescue403
     rescue_from ActiveRecord::RecordNotFound, with: :rescue404
     rescue_from ActionController::ParameterMissing, with: :rescue400
   end

2fab7e8313129dd1cf0e7147c19f0f0c8328193b
Masakazu_Harada <seungri.uhm@gmail.com>
2022-12-20 11:21:57 +0900

    skip_before_actionの実装でログインしていないアカウントを排除の実装

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,5 +1,6 @@
 class Staff::Base < ApplicationController
-
+  before_action :authorize
+  
   private
 
   def current_staff_member
@@ -10,4 +11,11 @@ class Staff::Base < ApplicationController
   end
 
   helper_method :current_staff_member
+
+  def authorize
+    unless current_staff_member
+      flash.alert = "職員としてログインしてください。"
+      redirect_to :staff_login
+    end
+  end
 end

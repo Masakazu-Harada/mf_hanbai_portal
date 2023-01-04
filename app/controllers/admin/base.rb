a8a3a1a96d648658b9c39cd06990ba16446922fc
Masakazu_Harada <seungri.uhm@gmail.com>
2023-01-04 17:21:53 +0900

    ログイン ログアウト記録のN + 1問題の解決の実装

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -3,7 +3,9 @@ class Admin::Base < ApplicationController
   before_action :check_account
   before_action :check_timeout
 
-  private def current_administrator
+  private 
+  
+  def current_administrator
     if session[:administrator_id]
       @current_administrator ||=
         Administrator.find_by(id: session[:administrator_id])
@@ -12,14 +14,14 @@ class Admin::Base < ApplicationController
 
   helper_method :current_administrator
 
-  private def authorize
+  def authorize
     unless current_administrator
       flash.alert = "管理者としてログインしてください。"
       redirect_to :admin_login
     end
   end
 
-  private def check_account
+  def check_account
     if current_administrator && current_administrator.suspended?
       session.delete(:administrator_id)
       flash.alert = "アカウントが無効になりました。"
@@ -29,8 +31,6 @@ class Admin::Base < ApplicationController
 
   TIMEOUT = 60.minutes
 
-  private 
-  
   def check_timeout
     if current_administrator
       if session[:admin_last_access_time] >= TIMEOUT.ago

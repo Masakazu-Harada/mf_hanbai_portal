68f9cd4b9acbd2359d9599e9cbe97fdac5d93c17
Masakazu_Harada <seungri.uhm@gmail.com>
2023-01-04 11:00:35 +0900

    Unknown action The action 'destroy' could not be found for Admin::SessionsControllerのエラー解決

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,4 @@
 class Admin::SessionsController < Admin::Base
-  skip_before_action :authorize
-
   def new
     if current_administrator
       redirect_to :admin_root
@@ -13,7 +11,7 @@ class Admin::SessionsController < Admin::Base
   def create
     @form = Admin::LoginForm.new(login_form_params)
     if @form.email.present?
-      administrator =
+      administrator = 
         Administrator.find_by("LOWER(email) = ?", @form.email.downcase)
     end
     if Admin::Authenticator.new(administrator).authenticate(@form.password)
@@ -22,7 +20,6 @@ class Admin::SessionsController < Admin::Base
         render action: "new"
       else
         session[:administrator_id] = administrator.id
-        session[:admin_last_access_time] = Time.current
         flash.notice = "ログインしました。"
         redirect_to :admin_root
       end
@@ -32,15 +29,15 @@ class Admin::SessionsController < Admin::Base
     end
   end
 
-  private 
-  
-  def login_form_params
-    params.require(:admin_login_form).permit(:email, :password)
-  end
-
   def destroy
     session.delete(:administrator_id)
     flash.notice = "ログアウトしました。"
     redirect_to :admin_root
+  end
+
+  private
+
+  def login_form_params
+    params.require(:admin_login_form).permit(:email, :password)
   end
 end

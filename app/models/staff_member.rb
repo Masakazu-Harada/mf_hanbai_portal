143fbf16f1aa34259592f3f2a460ed7f053643bb
Masakazu_Harada <seungri.uhm@gmail.com>
2023-01-05 23:00:43 +0900

    スタイルシートの変更と氏名と名前のフリガナのバリデーションをモデルに追加

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,6 +1,12 @@
 class StaffMember < ApplicationRecord
   has_many :events, class_name: "StaffEvent", dependent: :destroy #:staff_eventsではなく:eventsにした理由はメソッド名を短くしたかったとの事
 
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/ #全角カタカナと長棒(長音符)の正規表現を定数のKATAKANA_REGEPへ代入
+
+  validates :family_name, :given_name, presence: true #苗字と名前の空欄をはじく
+  validates :family_name_kana, :given_name_kana, presence: true, #フリガナの空欄をはじく
+    format: { with: KATAKANA_REGEXP, allow_blank: true } #正規表現にマッチするかどうかを調べるformatバリデーション。withオプションで正規表現を指定する。 allow_blankオプションをtrueにすると値が空の場合バリデーションをスキップする。
+
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)

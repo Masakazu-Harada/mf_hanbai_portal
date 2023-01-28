12bd375b479272acdf3fab58b9fd0eef11169aec
Masakazu_Harada <seungri.uhm@gmail.com>
2023-01-28 21:03:00 +0900

    単一テーブル継承のためのモデルの作成など

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_03_114105) do
+ActiveRecord::Schema.define(version: 2023_01_28_054920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.string "type", null: false
+    t.string "postal_code", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.string "address1", null: false
+    t.string "address2", null: false
+    t.string "company_name", default: "", null: false
+    t.string "division_name", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_addresses_on_customer_id"
+    t.index ["type", "customer_id"], name: "index_addresses_on_type_and_customer_id", unique: true
+  end
 
   create_table "administrators", force: :cascade do |t|
     t.string "email", null: false
@@ -24,10 +40,26 @@ ActiveRecord::Schema.define(version: 2023_01_03_114105) do
     t.index "lower((email)::text)", name: "index_administrators_on_LOWER_email", unique: true
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "family_name", null: false
+    t.string "given_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "given_name_kana", null: false
+    t.string "gender"
+    t.date "birthday"
+    t.string "hashed_password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index "lower((email)::text)", name: "index_customers_on_LOWER_email", unique: true
+    t.index ["family_name_kana", "given_name_kana"], name: "index_customers_on_family_name_kana_and_given_name_kana"
+  end
+
   create_table "staff_events", force: :cascade do |t|
     t.bigint "staff_member_id", null: false
     t.string "type", null: false
     t.datetime "created_at", null: false
+    t.integer "status"
     t.index ["created_at"], name: "index_staff_events_on_created_at"
     t.index ["staff_member_id", "created_at"], name: "index_staff_events_on_staff_member_id_and_created_at"
   end
@@ -48,5 +80,6 @@ ActiveRecord::Schema.define(version: 2023_01_03_114105) do
     t.index ["family_name_kana", "given_name_kana"], name: "index_staff_members_on_family_name_kana_and_given_name_kana"
   end
 
+  add_foreign_key "addresses", "customers"
   add_foreign_key "staff_events", "staff_members"
 end
